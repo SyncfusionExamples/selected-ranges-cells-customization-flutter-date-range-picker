c9b837d56cbc59ad354f7c70d0ad360c8e56ba8e
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-04 12:57:17 +0530

    FLUT-3117 - Sample and screenshots added.

---
+++ android/app/src/main/kotlin/com/example/lead_traildate_custom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lead_traildate_custom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(SelectedRangesCellsCustomization());
+
+class SelectedRangesCellsCustomization extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Card(
+              margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+              child: SfDateRangePicker(
+                view: DateRangePickerView.month,
+                selectionMode: DateRangePickerSelectionMode.multiRange,
+                selectionTextStyle: const TextStyle(color: Colors.green),
+                startRangeSelectionColor: Colors.pink,
+                endRangeSelectionColor: Colors.pink,
+                rangeSelectionColor: Colors.pinkAccent,
+                rangeTextStyle: const TextStyle(
+                    color: Colors.white, fontSize: 15),
+              )
+
+          ),
+        ),
+        // This trailing comma makes auto-formatting nicer for build methods.
+      ));
+  }
+}
